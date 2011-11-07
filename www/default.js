af480518260e38190df6aec898f2cb98c4d2441b
Garret Heaton <powdahound@gmail.com>
2011-11-06 20:18:17 -0800

    Initial version

---
+++ www/default.js
@@ -0,0 +1,67 @@
+// add parser through the tablesorter addParser method 
+$.tablesorter.addParser({ 
+    // set a unique id 
+    id: 'ioperf', 
+    is: function(s) { return false; }, 
+    format: function(s) { 
+        // format your data for normalization 
+        if (s == 'Low') {
+          return 0;
+        } else if (s == 'Moderate') {
+          return 1;
+        } else if (s == 'High') {
+          return 2;
+        } else {
+          return 3;
+        }
+    }, 
+    // set type, either numeric or text 
+    type: 'numeric' 
+});
+
+$(function() {
+  $('.tablesorter').tablesorter({
+    headers: {
+      /* memory */
+      1: {
+        sorter: 'digit'
+      },
+      /* compute units */
+      2: {
+        sorter: 'digit'
+      },
+      /* storage */
+      3: {
+        sorter: 'digit'
+      },
+      /* i/o perf */
+      5: {
+        sorter: 'ioperf'
+      }
+    },
+    // sortList: [[0,1]],
+    widgets: ['zebra']
+  });
+});
+
+$('#cost').bind('change', function(e) {
+  var selected = $('#cost option:selected').val();
+  var hour_multipliers = {
+    'hour': 1,
+    'day': 24,
+    'week': (7*24),
+    'month': (24*30),
+    'year': (365*24)
+  };
+  var multiplier = hour_multipliers[selected];
+  var per_hour;
+  var per_time;
+  $.each($('td.cost'), function(i, elem) {
+    elem = $(elem);
+    per_time = elem.attr('hour_cost');
+    if (selected != 'hour') {
+      per_time = (per_time * multiplier).toFixed(2);
+    }
+    elem.text('$' + per_time + ' per ' + selected);
+  });
+});
