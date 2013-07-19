63cf78ce2d251d52d23e0b2c7a118110b7a91497
Lee Parker <lee@spredfast.com>
2013-07-19 11:07:45 -0500

    Turn on regex for the search field

--- www/default.js
+++ www/default.js
@@ -59,6 +59,10 @@ $(function() {
       "bPaginate": false,
       "bInfo": false,
       "bStateSave": true,
+      "oSearch": {
+        "bRegex" : true,
+        "bSmart": false
+      },
       "aoColumnDefs": [
         {
           "aTargets": ["memory", "computeunits", "storage", "ioperf"],
@@ -74,7 +78,7 @@ $(function() {
         // because the cost duration may have changed while a filter was being
         // used and so some rows will need updating.
         change_cost(current_cost_duration);
-      }
+      } 
     });
   });
 
