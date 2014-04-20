a886458d831f865587fc42c394c4c3ec77f17012
Garret Heaton <powdahound@gmail.com>
2014-04-20 14:03:53 -0700

    PEP-8 fixes

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ import urllib2
 import re
 import json
 
+
 class Instance(object):
     def __init__(self):
         self.vpc = None
@@ -26,9 +27,11 @@ class Instance(object):
                                 size=self.drive_size)
         return d
 
+
 def totext(elt):
     s = etree.tostring(elt, method='text', encoding='unicode').strip()
     return re.sub(r'\*\d$', '', s)
+
 
 def parse_instance(tr):
     i = Instance()
@@ -48,7 +51,7 @@ def parse_instance(tr):
     if ecu == 'Variable':
         i.ECU = None
     else:
-        i.ECU  = float(ecu)
+        i.ECU = float(ecu)
     i.memory = float(totext(cols[5]))
     storage = totext(cols[6])
     m = re.search(r'(\d+)\s*x\s*([0-9,]+)?', storage)
@@ -61,10 +64,11 @@ def parse_instance(tr):
     else:
         assert storage == 'EBS only', "Unrecognized storage spec: %s" % (storage,)
         i.ebs_only = True
-    i.ebs_optimized = totext(cols[7]).lower()  == 'yes'
+    i.ebs_optimized = totext(cols[7]).lower() == 'yes'
     i.network_performance = totext(cols[8])
     print "Parsed %s..." % (i.instance_type)
     return i
+
 
 def scrape_instances():
     tree = etree.parse(urllib2.urlopen("http://aws.amazon.com/ec2/instance-types/"), etree.HTMLParser())
@@ -72,6 +76,7 @@ def scrape_instances():
     rows = details.xpath('tbody/tr')[1:]
     assert len(rows) > 0, "Didn't find any table rows."
     return [parse_instance(r) for r in rows]
+
 
 def transform_size(size):
     if size == 'u':
@@ -90,16 +95,17 @@ def transform_size(size):
     assert size == 'lg', "Unable to parse size: %s" % (size,)
     return 'large'
 
+
 def convert_to_type(typ, size):
     return size
+
 
 def transform_region(reg):
     region_map = {
         'eu-ireland': 'eu-west-1',
         'apac-sin': 'ap-southeast-1',
         'apac-syd': 'ap-southeast-2',
-        'apac-tokyo': 'ap-northeast-1',
-        }
+        'apac-tokyo': 'ap-northeast-1'}
     if reg in region_map:
         return region_map[reg]
     m = re.search(r'^([^0-9]*)(-(\d))?$', reg)
@@ -107,6 +113,7 @@ def transform_region(reg):
     base = m.group(1)
     num = m.group(3) or '1'
     return base + "-" + num
+
 
 def add_pricing(imap, data):
     for region_spec in data['config']['regions']:
@@ -117,7 +124,8 @@ def add_pricing(imap, data):
                 i_type = convert_to_type(typename, i_spec['size'])
                 # As best I can tell, this type doesn't exist, but is
                 # in the pricing charts anyways.
-                if i_type == 'cc2.4xlarge': continue
+                if i_type == 'cc2.4xlarge':
+                    continue
                 assert i_type in imap, "Unknown instance size: %s" % (i_type, )
                 inst = imap[i_type]
                 inst.pricing.setdefault(region, {})
@@ -125,10 +133,11 @@ def add_pricing(imap, data):
                 for col in i_spec['valueColumns']:
                     inst.pricing[region][col['name']] = col['prices']['USD']
 
+
 def add_pricing_data(instances):
     for i in instances:
         i.pricing = {}
-    by_type = {i.instance_type:i for i in instances}
+    by_type = {i.instance_type: i for i in instances}
 
     for platform in ['linux', 'mswin']:
         pricing_url = 'http://aws.amazon.com/ec2/pricing/json/%s-od.json' % (platform,)
@@ -136,12 +145,13 @@ def add_pricing_data(instances):
 
         add_pricing(by_type, pricing)
 
+
 def add_eni_info(instances):
     eni_url = "http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-eni.html"
     tree = etree.parse(urllib2.urlopen(eni_url), etree.HTMLParser())
     table = tree.xpath('//div[@id="divContent"]/div[@class="section"]//table[.//code[contains(., "cc2.8xlarge")]]')[0]
     rows = table.xpath('.//tr[./td]')
-    by_type = {i.instance_type:i for i in instances}
+    by_type = {i.instance_type: i for i in instances}
 
     for r in rows:
         instance_type = etree.tostring(r[0], method='text').strip()
@@ -152,8 +162,8 @@ def add_eni_info(instances):
             continue
         by_type[instance_type].vpc = {
             'max_enis': max_enis,
-            'ips_per_eni': ip_per_eni
-            }
+            'ips_per_eni': ip_per_eni}
+
 
 def scrape(data_file):
     """Scrape AWS to get instance data"""
