14412e8aa8d0da8beaab3d66ba41e0e23b5277cb
Maish Saidel-Keesing (msaidelk) <msaidelk@cisco.com>
2017-11-07 11:52:12 +0200

    added support for new C5 instances

--- scrape.py
+++ scrape.py
@@ -536,7 +536,7 @@ def add_linux_ami_info(instances):
 def add_vpconly_detail(instances):
     # specific instances can be lanuched in VPC only
     # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-vpc.html#vpc-only-instance-types
-    vpc_only_families = ('c4', 'f1', 'g3', 'i3', 'm4', 'p2', 'r4', 't2', 'x1')
+    vpc_only_families = ('c5', 'c4', 'f1', 'g3', 'i3', 'm4', 'p2', 'r4', 't2', 'x1')
     for i in instances:
         for family in vpc_only_families:
             if i.instance_type.startswith(family):
@@ -640,6 +640,7 @@ def add_pretty_names(instances):
         'r4': 'R4 High-Memory',
         'c3': 'C3 High-CPU',
         'c4': 'C4 High-CPU',
+        'c5': 'C5 High-CPU',
         'm3': 'M3 General Purpose',
         'i3': 'I3 High I/O',
         'cg1': 'Cluster GPU',
