7c7027e5d74d867b020559e4a3595cb8d80d4733
Garret Heaton <powdahound@gmail.com>
2017-12-29 10:53:23 -0800

    Add paris region to rds scraper

--- rds.py
+++ rds.py
@@ -74,6 +74,7 @@ def scrape(output_file, input_file=None):
         "EU (Frankfurt)": 'eu-central-1',
         "EU (Ireland)": 'eu-west-1',
         "EU (London)": 'eu-west-2',
+        "EU (Paris)": 'eu-west-3',
         "South America (Sao Paulo)": 'sa-east-1',
         "US East (N. Virginia)": 'us-east-1',
         "US East (Ohio)": 'us-east-2',
