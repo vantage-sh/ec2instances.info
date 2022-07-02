b766045d9aefb3045b5d287e2372954dda7b1e4f
Everett Berry <everett@vantage.sh>
2022-07-02 16:17:38 -0700

    load and organize instance data dynamically

--- in/instance-type.html.mako
+++ in/instance-type.html.mako
@@ -1,34 +1,51 @@
-<%!
-  active_ = "ec2"
-  import json
-  import six
-%>
-<%inherit file="base.mako" />
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <%block name="meta"/>
+    <link rel="stylesheet" href="/default.css" media="screen">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" media="screen">
+    <link rel="icon" type="image/png" href="/favicon.png">
+    <title>${i["Amazon"][4]['value']} Details</title>
+    <meta name="description" content="${i['Amazon'][4]['value']} CPUs, memory, storage and pricing"></head>
+    <style>
+      table, th, td {
+        border: 1px solid black;
+      }
+    </style>
+  </head>
     
-    <%block name="meta">
-        <title>${instance_type} Details</title>
-        <meta name="description" content="${instance_type} CPUs, memory, storage and pricing"></head>
-    </%block>
-
-    <%block name="header">
-    <h1>EC2Instances.info <small>Easy Amazon <b>ElastiCache</b> Instance Comparison</small></h1>
-    </%block>
-
-    <h1>${instance_type}</h1>
+  <body>
+    <h1>${i["Amazon"][4]['value']}</h1>
 
     <div>
-    % if instance_attrs:
-        <ul>
-            <li>Family Category: ${instance_attrs['family_category']}</li>
-            <li>CPUs: ${instance_attrs['cpu']}</li>
-            <li>CPU Type: ${instance_attrs['cpu_type']}</li>
-            <li>Memory: ${instance_attrs['memory']}</li>
-            <li>Storage: ${instance_attrs['storage_boot']}</li>
-            <li>Disk: ${instance_attrs['storage_extra']}</li>
-            <li>Network bandwidth: ${instance_attrs['network_bandwidth']}</li>
-            <li>Family Page: ${instance_attrs['family_url']}</li>
-        </ul>
-    % else:
-      <p>No data available</p>
-    % endif
+    % for category, attrs in i.items():
+      <h2>${category}</h2>
+        % if category == "Coming Soon":
+          % for a in attrs:
+            <p>${a["display_name"]}</p>
+          % endfor
+        % elif category == "Not Shown":
+          <p>Request this data to be included on Github</p>
+          % for a in attrs:
+            <p>${a["cloud_key"]}</p>
+          % endfor
+        % else:
+          <table>
+            <tr>
+              <th>${category}</th>
+              <th></th>
+            </tr>
+          % for a in attrs:
+            <tr>
+              <td>${a["display_name"]}</td>
+              <td>${a["value"]}</td>
+            </tr>
+          % endfor
+          </table>
+        % endif
+    % endfor
     </div>
+  </body>
+</html>
--- render.py
+++ render.py
@@ -8,6 +8,156 @@ import os
 import csv
 
 
+
+
+def add_render_info(i):
+    i["network_sort"] = network_sort(i)
+    add_cpu_detail(i)
+
+
+def storage(attrs):
+    pass
+
+
+def availability(attrs):
+    pass
+
+
+def vpc(attrs):
+    pass
+
+
+def prices(attrs):
+    # price = json.dumps(i[j]['us-east-1'], indent=4)
+    # print(price)
+    pass
+
+
+def route_special_cases(expanded_instance_attr):
+    subcategory = expanded_instance_attr[0]
+
+    if subcategory == "availability_zones":
+        availability(expanded_instance_attr[3])
+    elif subcategory == "vpc":
+        vpc(expanded_instance_attr[3])
+    elif subcategory == "storage":
+        storage(expanded_instance_attr[3])
+
+
+def load_service_attributes():
+    special_attrs = [
+        "availability_zones",
+        "vpc",
+        "storage",
+        "pricing",
+    ]
+    data_file = 'service-attributes-ec2.csv'
+
+    display_map = {}
+    with open(data_file, 'r') as f:
+        reader = csv.reader(f)
+
+        for i, row in enumerate(reader):
+            if i == 0:
+                continue
+            elif row[0] in special_attrs:
+                route_special_cases(row)
+                display_map[row[0]] = {
+                    "display_name": row[1],
+                    "category": "Coming Soon"
+                }
+            else:
+                display_map[row[0]] = {
+                    "display_name": row[1],
+                    "category": row[2],
+                }
+            
+    return display_map
+
+
+def load_service_attributes_cloudhw(data_file):
+    # Transform a CSV of instance attributes into a dict of dicts for later lookup
+
+    instance_lookup = {}
+    with open(data_file, 'r') as f:
+        reader = csv.reader(f)
+        header = []
+
+        for i, row in enumerate(reader):
+            if i == 0:
+                header = row
+                continue
+            single_inst = {}
+            for key, val in zip(header, row):
+                single_inst[key] = val
+            instance_lookup[row[4]] = single_inst
+
+    return instance_lookup
+
+
+def map_ec2_attributes(i):
+    # For now, manually transform the instance data we receive from AWS 
+    # into the format we want to render. Later we can create this in YAML
+    # and use a standard function that maps names
+    categories = [
+        "Amazon",
+        "Compute",
+        "Networking",
+        "Storage",
+        "Hardware",
+        "Not Shown",
+        "Coming Soon",
+    ]
+    instance_details = {}
+    for c in categories:
+        instance_details[c] = []
+
+    imap = load_service_attributes()
+    for j, k in i.items():
+
+        display = imap[j]
+        display["value"] = k
+        display["cloud_key"] = j
+        instance_details[display["category"]].append(display)
+    
+    # Sort the instance attributes in each category alphabetically,
+    # another general-purpose option could be to sort by value data type
+    for c in categories:
+        instance_details[c].sort(key=lambda x: x["display_name"])
+
+    return instance_details
+
+
+def build_instance_families(instances, destination_file):
+    # Find URL path (service) for these instances. It's / for ec2
+    subdir = 'www'
+    dest_subdir = destination_file.split('/')[1]
+    if dest_subdir != 'index.html':
+        subdir = os.path.join('www', dest_subdir)
+
+    lookup = mako.lookup.TemplateLookup(directories=["."])
+    template = mako.template.Template(filename='in/instance-type.html.mako', lookup=lookup)
+    instance_families = {}
+    for i in instances:
+        # In case of emergency: print(json.dumps(i, indent=4))
+
+        if "instance_type" in i:
+            instance_type = i["instance_type"]
+            if instance_type not in instance_families:
+                instance_families[instance_type] = {
+                    "instance_type": instance_type,
+                    "instance_data": {},
+                }
+                instance_page = os.path.join(subdir, instance_type + '.html')
+                instance_details = map_ec2_attributes(i)
+
+                with io.open(instance_page, "w+", encoding="utf-8") as fh:
+                    try:
+                        fh.write(template.render(i=instance_details))
+                    except:
+                        print(mako.exceptions.text_error_template().render())
+
+
 def network_sort(inst):
     perf = inst["network_performance"]
     network_rank = [
@@ -58,70 +208,6 @@ def add_cpu_detail(i):
         i["intel_turbo"] = "Yes" if i["intel_turbo"] else ""
 
 
-def add_render_info(i):
-    i["network_sort"] = network_sort(i)
-    add_cpu_detail(i)
-
-
-
-def get_instance_attributes(data_file):
-    # Transform a CSV of instance attributes into a dict of dicts for later lookup
-
-    instance_lookup = {}
-    with open(data_file, 'r') as f:
-        reader = csv.reader(f)
-        header = []
-
-        for i, row in enumerate(reader):
-            if i == 0:
-                header = row
-                continue
-            single_inst = {}
-            for key, val in zip(header, row):
-                single_inst[key] = val
-            instance_lookup[row[4]] = single_inst
-
-    return instance_lookup
-
-
-def build_instance_families(instances, destination_file, pricing_json, instance_azs_json, generated_at):
-    # Find URL path (service) for these instances. It's / for ec2
-    subdir = 'www'
-    dest_subdir = destination_file.split('/')[1]
-    if dest_subdir != 'index.html':
-        subdir = os.path.join('www', dest_subdir)
-
-    idetails = get_instance_attributes("www/instance-types.csv")
-
-    lookup = mako.lookup.TemplateLookup(directories=["."])
-    template = mako.template.Template(filename='in/instance-type.html.mako', lookup=lookup)
-    instance_families = {}
-    for i in instances:
-        if "instance_type" in i:
-            instance_type = i["instance_type"]
-            if instance_type not in instance_families:
-                instance_families[instance_type] = {
-                    "instance_type": instance_type,
-                    "instance_data": {},
-                }
-                instance_page = os.path.join(subdir, instance_type + '.html')
-                attrs = idetails.get(instance_type)
-
-                with io.open(instance_page, "w+", encoding="utf-8") as fh:
-                    try:
-                        fh.write(
-                        template.render(
-                            instance_type=instance_type,
-                            pricing_json=pricing_json,
-                            generated_at=generated_at,
-                            instance_azs_json=instance_azs_json,
-                            instance_attrs=attrs,
-                        )
-                        )
-                    except:
-                        print(mako.exceptions.text_error_template().render())
-
-
 prices_dict = {}
 prices_index = 0
 
@@ -169,20 +255,18 @@ def render(data_file, template_file, destination_file):
     """Build the HTML content from scraped data"""
     lookup = mako.lookup.TemplateLookup(directories=["."])
     template = mako.template.Template(filename=template_file, lookup=lookup)
-    print("Loading data from %s..." % data_file)
     with open(data_file) as f:
         instances = json.load(f)
+
+    print("Loading data from %s..." % data_file)
     for i in instances:
         add_render_info(i)
-
-
     pricing_json = compress_pricing(instances)
-    instance_azs_json = compress_instance_azs(instances)
-    print("Rendering to %s..." % destination_file)
     generated_at = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+    instance_azs_json = compress_instance_azs(instances)
+    build_instance_families(instances, destination_file)
 
-    build_instance_families(instances, destination_file, pricing_json, instance_azs_json, generated_at)
-
+    print("Rendering to %s..." % destination_file)
     os.makedirs(os.path.dirname(destination_file), exist_ok=True)
     with io.open(destination_file, "w+", encoding="utf-8") as fh:
         try:
@@ -200,5 +284,5 @@ def render(data_file, template_file, destination_file):
 
 if __name__ == "__main__":
     render("www/instances.json", "in/index.html.mako", "www/index.html")
-    render("www/rds/instances.json", "in/rds.html.mako", "www/rds/index.html")
-    render("www/cache/instances.json", "in/cache.html.mako", "www/cache/index.html")
+    # render("www/rds/instances.json", "in/rds.html.mako", "www/rds/index.html")
+    # render("www/cache/instances.json", "in/cache.html.mako", "www/cache/index.html")
