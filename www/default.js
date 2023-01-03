eb0f7925cd964d55f318fb137722fe5a5f931fcf
Everett Berry <everett@vantage.sh>
2023-01-02 21:22:57 -0500

    track deselected rows

--- www/default.js
+++ www/default.js
@@ -863,6 +863,14 @@ function configure_highlighting() {
     }
 
     $(this).toggleClass('highlight');
+    
+    // remove a deselected row from the list of selected rows
+    if(!$(this).hasClass('highlight')) {
+      var selected = g_settings.selected.split(',');
+      const index = selected.indexOf($(this).attr("id"));
+      selected.splice(index);
+      g_settings.selected = selected.join();
+    }
 
     update_compare_button();
     maybe_update_url();
