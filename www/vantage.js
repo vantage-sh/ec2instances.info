8ab4b779c2e7cbc622d8b6515cc2a32138a7c580
Colin Keany <colin@vantage.sh>
2024-06-14 11:02:24 -0400

    remove js

--- www/vantage.js
+++ www/vantage.js
@@ -1,13 +1,5 @@
 'use strict';
 
-const redirectUrls = [
-  "https://instances.vantage.sh/#1",
-  "https://instances.vantage.sh/#2",
-  "https://instances.vantage.sh/#3",
-]
-
-document.location = redirectUrls[Math.floor(Math.random() * redirectUrls.length)]
-
 $(document).ready(function () {
   var vantage_settings = store.get('vantage');
   
