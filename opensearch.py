84b95bf1eb83229dda44f0e0f63125009e218c07
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2024-10-31 11:44:05 +0100

    Fix crash in OpenSearch scraper caused by missing instance type key for Serverless and other SKU types

--- opensearch.py
+++ opensearch.py
@@ -114,29 +114,30 @@ def scrape(output_file, input_file=None):
 
     # loop through products, and only fetch available instances for now
     for sku, product in tqdm(six.iteritems(data["products"])):
-        if (
-            product.get("productFamily", None) == "Amazon OpenSearch Service Instance"
+        if (product.get("productFamily", None) == "Amazon OpenSearch Service Instance"
             and product.get("attributes", {}).get("operation", None)
-            != "DirectQueryAmazonS3GDCOCU"
-        ):
-            attributes = product["attributes"]
+            != "DirectQueryAmazonS3GDCOCU"):
 
-            if "instanceType" in attributes:
-                # map the region
-                location = ec2.canonicalize_location(attributes["location"])
-                instance_type = attributes["instanceType"]
-                if location == "Any":
-                    region = "us-east-1"
-                elif location == "Asia Pacific (Osaka-Local)":
-                    # at one point this region was local but was upgraded to a standard region
-                    # however some SKUs still reference the old region
-                    region = "ap-northeast-3"
-                    regions[location] = region
-                elif location not in regions.values():
-                    region = attributes["regionCode"]
-                    regions[location] = region
-                else:
-                    region = regions[location]
+            attributes = product.get("attributes", {})
+            if "instanceType" not in attributes:
+                continue
+
+            instance_type = attributes["instanceType"]
+
+            # map the region
+            location = ec2.canonicalize_location(attributes["location"])
+            if location == "Any":
+                region = "us-east-1"
+            elif location == "Asia Pacific (Osaka-Local)":
+                # at one point this region was local but was upgraded to a standard region
+                # however some SKUs still reference the old region
+                region = "ap-northeast-3"
+                regions[location] = region
+            elif location not in regions.values():
+                region = attributes["regionCode"]
+                regions[location] = region
+            else:
+                region = regions[location]
 
                 # set the attributes in line with the ec2 index
                 attributes["region"] = region
@@ -148,20 +149,18 @@ def scrape(output_file, input_file=None):
 
                 caches_instances[sku] = attributes
 
-                if instance_type not in instances.keys():
-                    # delete some attributes that are inconsistent among skus
-                    new_attributes = (
-                        attributes.copy()
-                    )  # make copy so we can keep these attributes with the sku
-                    new_attributes.pop("location", None)
-                    new_attributes.pop("locationType", None)
-                    new_attributes.pop("operation", None)
-                    new_attributes.pop("region", None)
-                    new_attributes.pop("usagetype", None)
-                    new_attributes["pricing"] = attributes["pricing"]
-                    new_attributes["regions"] = {}
+            if instance_type not in instances.keys():
+                # delete some attributes that are inconsistent among skus
+                new_attributes = attributes.copy()  # make copy so we can keep these attributes with the sku
+                new_attributes.pop("location", None)
+                new_attributes.pop("locationType", None)
+                new_attributes.pop("operation", None)
+                new_attributes.pop("region", None)
+                new_attributes.pop("usagetype", None)
+                new_attributes["pricing"] = attributes["pricing"]
+                new_attributes["regions"] = {}
 
-                    instances[instance_type] = new_attributes
+               instances[instance_type] = new_attributes
 
     # Parse ondemand pricing
     for sku, offers in six.iteritems(data["terms"]["OnDemand"]):
@@ -292,9 +291,7 @@ def scrape(output_file, input_file=None):
                             "yrTerm3.noUpfront-hrs"
                         ]
 
-                    instances[instance_type]["pricing"][region][
-                        "reserved"
-                    ] = reserved_prices
+                    instances[instance_type]["pricing"][region]["reserved"] = reserved_prices
                 except Exception as e:
                     print(
                         "ERROR: Trouble generating Cache reserved price for {}: {!r}".format(
