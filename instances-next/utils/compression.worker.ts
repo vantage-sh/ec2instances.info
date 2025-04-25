99a51d765a244737858007c9689315f4bbb2d6d6
Astrid Gealer <astrid@gealer.email>
2025-04-25 16:14:00 +0100

    use web workers for perf

---
+++ instances-next/utils/compression.worker.ts
@@ -0,0 +1,29 @@
+import { Instance } from "@/types";
+import { decodeArrayStream } from "@msgpack/msgpack";
+import { XzReadableStream } from "xz-decompress";
+
+onmessage = async (e) => {
+    const { url }: { url: string } = e.data;
+
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Failed to fetch compressed file: ${url}`);
+    }
+
+    // Decompress the instances.
+    const s = new XzReadableStream(res.body!);
+    let instancesBuffer: Instance[] = [];
+    try {
+        for await (const item of decodeArrayStream(s)) {
+            instancesBuffer.push(item as Instance);
+            if (instancesBuffer.length === 50) {
+                postMessage(instancesBuffer);
+                instancesBuffer = [];
+            }
+        }
+    } catch {
+        // At the end it throws an error for some reason. It does get all
+        // the instances though, so I'm not too worried.
+    }
+    if (instancesBuffer.length > 0) postMessage(instancesBuffer);
+};
--- instances-next/utils/handleCompressedFile.ts
+++ instances-next/utils/handleCompressedFile.ts
@@ -1,45 +1,33 @@
 import { Instance } from "@/types";
-import { decodeArrayStream } from "@msgpack/msgpack";
-import { XzReadableStream } from "xz-decompress";
 
 export default function handleCompressedFile(path: string, instances: Instance[]) {
+    if (!window.Worker) {
+        return {
+            get value() {
+                return instances;
+            },
+            addChangeNotifier: (fn: () => void) => {
+                return () => {};
+            },
+        };
+    }
+
+    const worker = new Worker(new URL("./compression.worker.ts", import.meta.url), {
+        type: "module",
+    });
+
     const changeNotifier = new Set<() => void>();
+    worker.onmessage = (e) => {
+        const newInstances = e.data as Instance[];
+        instances = [...instances, ...newInstances];
+        for (const fn of changeNotifier) {
+            fn();
+        }
+    };
 
-    (async () => {
-        const res = await fetch(path);
-        if (!res.ok) {
-            throw new Error(`Failed to fetch compressed file: ${path}`);
-        }
-
-        // Decompress the instances.
-        const s = new XzReadableStream(res.body!);
-        let instancesBuffer: Instance[] = [];
-        try {
-            for await (const item of decodeArrayStream(s)) {
-                instancesBuffer.push(item as Instance);
-                if (instancesBuffer.length === 30) {
-                    instances = [...instances, ...instancesBuffer];
-                    instancesBuffer = [];
-                    setTimeout(() => {
-                        for (const fn of changeNotifier) {
-                            fn();
-                        }
-                    }, 0);
-                }
-            }
-        } catch {
-            // At the end it throws an error for some reason. It does get all
-            // the instances though, so I'm not too worried.
-        }
-        if (instancesBuffer.length > 0) {
-            instances = [...instances, ...instancesBuffer];
-            setTimeout(() => {
-                for (const fn of changeNotifier) {
-                    fn();
-                }
-            }, 0);
-        }
-    })();
+    worker.postMessage({
+        url: new URL(path, `${window.location.protocol}//${window.location.host}`).href,
+    });
 
     return {
         get value() {
