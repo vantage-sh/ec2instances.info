2f8e761d58fe4843c127f65b993f9cd453ec9ae2
Astrid Gealer <astrid@gealer.email>
2025-04-26 13:30:01 +0100

    more optimisations

--- instances-next/utils/compression.worker.ts
+++ instances-next/utils/compression.worker.ts
@@ -1,4 +1,4 @@
-import { Instance } from "@/types";
+import { Instance, Pricing } from "@/types";
 import { decodeArrayStream } from "@msgpack/msgpack";
 import { XzReadableStream } from "xz-decompress";
 
@@ -13,9 +13,60 @@ onmessage = async (e) => {
     // Decompress the instances.
     const s = new XzReadableStream(res.body!);
     let instancesBuffer: Instance[] = [];
+    let pricingRainbowTable: Map<number, string> | null = null;
+    const processRainbowTable = (instance: Instance) => {
+        // @ts-expect-error: This is intentionally the wrong type.
+        const pricing: [
+            // Region
+            number,
+            [
+                // Platform
+                number,
+                [
+                    // Key inside platform
+                    number,
+                    any,
+                ][],
+            ][],
+        ][] = instance.pricing;
+        const newPricing: Pricing = {};
+        for (const region of pricing) {
+            const regionKey = pricingRainbowTable!.get(region[0]);
+            const platforms: Pricing[string] = {};
+            for (const platform of region[1]) {
+                const platformKey = pricingRainbowTable!.get(platform[0]);
+                const kv: { [key: string]: any } = {};
+                for (const v of platform[1]) {
+                    const key = pricingRainbowTable!.get(v[0])!;
+                    if (key === "reserved") {
+                        const reserved: any = {};
+                        for (const reservedKv of v[1]) {
+                            reserved[pricingRainbowTable!.get(reservedKv[0])!] = reservedKv[1];
+                        }
+                        kv[key] = reserved;
+                    } else {
+                        kv[key] = v[1];
+                    }
+                }
+                // @ts-expect-error: This is intentionally the wrong type.
+                platforms[platformKey] = kv;
+            }
+            newPricing[regionKey!] = platforms;
+        }
+        instance.pricing = newPricing;
+        return instance;
+    };
     try {
         for await (const item of decodeArrayStream(s)) {
-            instancesBuffer.push(item as Instance);
+            if (pricingRainbowTable === null) {
+                const arr = item as string[];
+                pricingRainbowTable = new Map<number, string>();
+                for (let i = 0; i < arr.length; i++) {
+                    pricingRainbowTable.set(i, arr[i]);
+                }
+                continue;
+            }
+            instancesBuffer.push(processRainbowTable(item as Instance));
             if (instancesBuffer.length === 50) {
                 postMessage(instancesBuffer);
                 instancesBuffer = [];
--- instances-next/utils/conpressAwsData.ts
+++ instances-next/utils/conpressAwsData.ts
@@ -56,6 +56,71 @@ function addRenderInfo(instance: Instance) {
     addCpuDetail(instance);
 }
 
+function makeRainbowTable(instances: Instance[]) {
+    // Pass 1: Get all the keys.
+    const pricingSet = new Set<string>();
+    for (const instance of instances) {
+        const pricing = instance.pricing;
+        for (const region in pricing) {
+            pricingSet.add(region);
+            for (const platform in pricing[region]) {
+                pricingSet.add(platform);
+                for (const key in pricing[region][platform]) {
+                    pricingSet.add(key);
+                }
+                for (const term in pricing[region][platform].reserved || {}) {
+                    pricingSet.add(term);
+                }
+            }
+        }
+    }
+
+    // Pass 2: Mutate the instances to use the rainbow table.
+    const rainbowTable = Array.from(pricingSet);
+    for (const instance of instances) {
+        const pricing = instance.pricing;
+        const newPricing: [
+            // Region
+            number,
+            [
+                // Platform
+                number,
+                [
+                    // Key inside platform
+                    number,
+                    any,
+                ][],
+            ][],
+        ][] = [];
+        for (const region in pricing) {
+            const regionIndex = rainbowTable.indexOf(region);
+            const platforms: [number, [number, any][]][] = [];
+            for (const platform in pricing[region]) {
+                const platformIndex = rainbowTable.indexOf(platform);
+                const kv: [number, any][] = [];
+                for (const key in pricing[region][platform]) {
+                    const keyIndex = rainbowTable.indexOf(key);
+                    if (key === "reserved") {
+                        const reserved: [number, string][] = [];
+                        for (const [key, value] of Object.entries(pricing[region][platform].reserved || {})) {
+                            reserved.push([rainbowTable.indexOf(key), value]);
+                        }
+                        kv.push([keyIndex, reserved]);
+                    } else {
+                        // @ts-expect-error: Doing some dangerous type stuff here.
+                        kv.push([keyIndex, pricing[region][platform][key]]);
+                    }
+                }
+                platforms.push([platformIndex, kv]);
+            }
+            newPricing.push([regionIndex, platforms]);
+        }
+        // @ts-expect-error: Doing some dangerous type stuff here.
+        instance.pricing = newPricing;
+    }
+    return [rainbowTable, ...instances];
+}
+
 async function main() {
     const regions: Region = {
         main: {},
@@ -80,9 +145,9 @@ async function main() {
 
     // Encode and then compress the instances.
     const first50Instances = instances.slice(0, 50);
-    const remainingInstances = instances.slice(50);
     const first50InstancesEncoded = encode(first50Instances);
-    const remainingInstancesEncoded = encode(remainingInstances);
+    const remainingInstances = instances.slice(50);
+    const remainingInstancesEncoded = encode(makeRainbowTable(remainingInstances));
     console.log("Compressing AWS instances data...");
     const compressedRemainingInstances: Buffer = await new Promise((res) => {
         compress(Buffer.from(remainingInstancesEncoded), {}, (result) => {
