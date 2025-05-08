1cd7a1c09433ea081f215d547622c887053ba43d
Astrid Gealer <astrid@gealer.email>
2025-05-08 22:35:43 +0100

    fix build error

--- instances-next/app/aws/rds/[slug]/page.tsx
+++ instances-next/app/aws/rds/[slug]/page.tsx
@@ -2,10 +2,8 @@ import EC2InstanceRoot from "@/components/EC2InstanceRoot";
 import { EC2Instance, Region } from "@/types";
 import bestEc2InstanceForEachVariant from "@/utils/bestEc2InstanceForEachVariant";
 import makeRainbowTable from "@/utils/makeRainbowTable";
-import processRainbowTable from "@/utils/processRainbowTable";
 import { decode } from "@msgpack/msgpack";
 import { readFile } from "fs/promises";
-import { XzReadableStream } from "xz-decompress";
 
 export const dynamic = "force-static";
 
@@ -17,49 +15,19 @@ async function getData() {
         const regions = decode(
             await readFile("./public/instance-rds-regions.msgpack"),
         ) as Region;
-        const compressed30 = decode(
-            await readFile("./public/first-30-rds-instances.msgpack"),
+        const instances = JSON.parse(
+            await readFile("../www/rds/instances.json", "utf-8"),
         ) as EC2Instance[];
-
-        const compressed = await readFile("./public/remaining-rds-instances.msgpack.xz");
-        const stream = new XzReadableStream(
-            new ReadableStream({
-                start(controller) {
-                    controller.enqueue(compressed);
-                    controller.close();
-                },
-            }),
-        );
-        const chunks: Uint8Array[] = [];
-        const reader = stream.getReader();
-        while (true) {
-            const { done, value } = await reader.read();
-            if (done) break;
-            chunks.push(value);
+        for (const x of instances) {
+            if ("vcpu" in x) {
+                // @ts-expect-error: This is a different typed field.
+                x.vCPU = x.vcpu;
+                delete x.vcpu;
+            }
         }
-        const remaining = decode(Buffer.concat(chunks)) as EC2Instance[];
-        // @ts-expect-error: The first item is the rainbow table.
-        const rainbowTable: string[] = remaining.shift();
-        const remainingInstances = remaining.map((i) =>
-            processRainbowTable(rainbowTable, i),
-        );
-        // @ts-expect-error: The first item is the rainbow table.
-        const first30RainbowTable: string[] = compressed30.shift();
         return {
             regions,
-            instances: [
-                ...compressed30.map((i) =>
-                    processRainbowTable(first30RainbowTable, i),
-                ),
-                ...remainingInstances,
-            ].map((x) => {
-                if ("vcpu" in x) {
-                    // @ts-expect-error: This is a different typed field.
-                    x.vCPU = x.vcpu;
-                    delete x.vcpu;
-                }
-                return x;
-            }),
+            instances,
         };
     })();
     return p;
