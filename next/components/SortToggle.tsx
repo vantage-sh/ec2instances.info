789010d24e5cf87de664bf41f5f06f21a21819f6
Astrid Gealer <astrid@gealer.email>
2025-05-12 21:52:26 +0100

    remove aria-label since there is a title now

--- next/components/SortToggle.tsx
+++ next/components/SortToggle.tsx
@@ -11,8 +11,9 @@ export default function SortToggle({ value, setValue }: SortToggleProps) {
     return (
         <div className="ml-1 absolute right-1 top-0 inline-block">
             <button
-                className={`${value === false ? "text-purple-nu" : "text-gray-4"} p-0 text-xs block cursor-pointer hover:text-gray-3 overflow-hidden w-4 h-3`}
-                aria-label="Sort ascending"
+                className={`${
+                    value === false ? "text-purple-nu" : "text-gray-4"
+                } p-0 text-xs block cursor-pointer hover:text-gray-3 overflow-hidden w-4 h-3`}
                 title="Sort ascending"
                 aria-pressed={value === false}
                 onClick={() => setValue(false)}
@@ -20,8 +21,9 @@ export default function SortToggle({ value, setValue }: SortToggleProps) {
                 <ChevronUpIcon className="relative w-4 h-4" />
             </button>
             <button
-                className={`${value === true ? "text-purple-nu" : "text-gray-4"} p-0 text-xs block cursor-pointer hover:text-gray-3 overflow-hidden w-4 h-3 ml-[5px]`}
-                aria-label="Sort descending"
+                className={`${
+                    value === true ? "text-purple-nu" : "text-gray-4"
+                } p-0 text-xs block cursor-pointer hover:text-gray-3 overflow-hidden w-4 h-3 ml-[5px]`}
                 title="Sort descending"
                 aria-pressed={value === true}
                 onClick={() => setValue(true)}
