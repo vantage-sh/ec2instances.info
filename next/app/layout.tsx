208bb2052c2f16c88fb435f9fd738d5b19703553
Astrid Gealer <astrid@gealer.email>
2025-05-12 16:48:00 +0100

    add favicon

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -11,6 +11,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/favicon.png" />
+            </head>
             <body className={inter.className}>
                 <TopNav />
                 {children}
