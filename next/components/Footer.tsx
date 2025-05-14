640e1e5d611089351192ee2a9cc6fad297d9d510
Astrid Gealer <astrid@gealer.email>
2025-05-14 14:43:40 +0100

    change input metadata

--- next/components/Footer.tsx
+++ next/components/Footer.tsx
@@ -42,6 +42,8 @@ export default function Footer() {
                                 className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                                 name="authentication_email"
                                 placeholder="Email"
+                                type="email"
+                                required
                             />
                         </div>
                         <div className="hidden md:block">
