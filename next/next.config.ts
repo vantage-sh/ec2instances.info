b38870e31dc3bb1b462443d4a0111d101be5d25b
Astrid Gealer <astrid@gealer.email>
2025-05-14 15:17:08 +0100

    make tsc run in parallel

--- next/next.config.ts
+++ next/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     output: "export",
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
