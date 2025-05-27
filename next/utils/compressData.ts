02df96115d019b39634b415b47ef2db191c4124d
Astrid Gealer <astrid@gealer.email>
2025-05-27 22:32:23 +0100

    fix bug with the regex logic re local vs main zones

--- next/utils/compressData.ts
+++ next/utils/compressData.ts
@@ -28,7 +28,7 @@ async function compressEC2Instances() {
         for (const r in instance.pricing) {
             if (r.includes("wl1") || r.includes("wl2")) {
                 regions.wavelength[r] = instance.regions[r];
-            } else if (/\d+/.test(r)) {
+            } else if ((r.match(/\d+/g) || []).length > 1) {
                 regions.local_zone[r] = instance.regions[r];
             } else {
                 regions.main[r] = instance.regions[r];
@@ -167,7 +167,7 @@ async function compressRDSInstances() {
         for (const r in instance.pricing) {
             if (r.includes("wl1") || r.includes("wl2")) {
                 regions.wavelength[r] = instance.regions[r];
-            } else if (/\d+/.test(r)) {
+            } else if ((r.match(/\d+/g) || []).length > 1) {
                 regions.local_zone[r] = instance.regions[r];
             } else {
                 regions.main[r] = instance.regions[r];
