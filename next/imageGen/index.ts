57861bcf633af9bbd16baa627580807c686b565b
Astrid Gealer <astrid@gealer.email>
2025-05-28 22:11:36 +0100

    VERY early build

---
+++ next/imageGen/index.ts
@@ -0,0 +1,34 @@
+import makeImage from "./makeImage";
+
+makeImage(
+    "EC2 Instances",
+    "t1.micro",
+    [
+        {
+            name: "CPU",
+            value: "test hello test",
+            squareIconPath: "icons/cpu.png",
+        },
+        {
+            name: "CPU",
+            value: "test hello test",
+            squareIconPath: "icons/cpu.png",
+        },
+        {
+            name: "CPU",
+            value: "test hello test",
+            squareIconPath: "icons/cpu.png",
+        },
+        {
+            name: "CPU",
+            value: "test hello test",
+            squareIconPath: "icons/cpu.png",
+        },
+        {
+            name: "CPU",
+            value: "test hello test",
+            squareIconPath: "icons/cpu.png",
+        },
+    ],
+    "./test.png",
+);
