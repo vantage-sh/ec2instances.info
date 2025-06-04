df0ec89340365cca879e7766d694fe65859d2733
Astrid Gealer <astrid@gealer.email>
2025-06-04 12:06:46 +0100

    Add Sentry (fixes #815)
    Sentry gives us a lot of useful information when it comes to debugging and is widely seen as a trusted tool for software development. Using Sentry in this project is optional and if it is unset it will be tree shaken out.

---
+++ next/app/instrumentation-client.ts
@@ -0,0 +1,26 @@
+let importedCaptureRouterTransitionStart: (
+    href: string,
+    navigationType: string,
+) => void | undefined;
+
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+    import("@sentry/nextjs").then(({ init, captureRouterTransitionStart }) => {
+        init({
+            dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+            // Set the sample rate to 10%. With our user rate, this is still a
+            // lot of useful information.
+            tracesSampleRate: 0.1,
+        });
+        importedCaptureRouterTransitionStart = captureRouterTransitionStart;
+    });
+}
+
+export const onRouterTransitionStart = (
+    href: string,
+    navigationType: string,
+) => {
+    if (importedCaptureRouterTransitionStart) {
+        importedCaptureRouterTransitionStart(href, navigationType);
+    }
+};
