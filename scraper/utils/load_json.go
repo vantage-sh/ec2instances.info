5370e78ed3afdfaf8eb27f32a09b5c9704a329ca
Astrid Gealer <astrid@gealer.email>
2025-07-16 17:46:39 +0100

    lots more logic, lots of the azure stuff

--- scraper/utils/load_json.go
+++ scraper/utils/load_json.go
@@ -10,14 +10,19 @@ import (
 	"time"
 )
 
-// LoadJson loads a JSON file from the given URL and unmarshals it into the given value.
-func LoadJson(url string, val any) error {
+// LoadJsonWithBearerToken loads a JSON file from the given URL and unmarshals it into the given value.
+// If the bearer token is nil, it will not be added to the request.
+func LoadJsonWithBearerToken(url string, val any, bearerToken *string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*15)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return err
+	}
+
+	if bearerToken != nil {
+		req.Header.Set("Authorization", "Bearer "+*bearerToken)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
@@ -27,7 +32,11 @@ func LoadJson(url string, val any) error {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("status code %d for url %s", resp.StatusCode, url)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("status code %d for url %s: %w", resp.StatusCode, url, err)
+		}
+		return fmt.Errorf("status code %d for url %s: %s", resp.StatusCode, url, string(body))
 	}
 
 	body, err := io.ReadAll(resp.Body)
@@ -38,4 +47,9 @@ func LoadJson(url string, val any) error {
 	log.Default().Printf("Loaded %d bytes from %s", len(body), url)
 
 	return json.Unmarshal(body, val)
+}
+
+// LoadJson loads a JSON file from the given URL and unmarshals it into the given value.
+func LoadJson(url string, val any) error {
+	return LoadJsonWithBearerToken(url, val, nil)
 }
