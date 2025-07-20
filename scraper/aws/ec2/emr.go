9619e22e6e69091c2bf62737e31e70b8274c3149
Astrid Gealer <astrid@gealer.email>
2025-07-20 16:46:20 +0100

    add emr to china

--- scraper/aws/ec2/emr.go
+++ scraper/aws/ec2/emr.go
@@ -2,8 +2,12 @@ package ec2
 
 import (
 	"log"
+	"regexp"
 	"scraper/aws/awsutils"
+	"scraper/utils"
 	"strings"
+
+	"github.com/anaskhan96/soup"
 )
 
 type emrPrice struct {
@@ -14,14 +18,109 @@ type emrData struct {
 	Regions map[string]map[string]emrPrice `json:"regions"`
 }
 
+var EMR_PRICE_REGEX = regexp.MustCompile(`\d+\.\d+`)
+
 func addEmrPricingCn(instances map[string]*EC2Instance, regionsInverted map[string]string) {
-	// TODO: Add China
+	log.Default().Println("Adding EMR pricing to EC2 (China)")
+
+	doc, err := utils.LoadHTML("https://www.amazonaws.cn/en/elasticmapreduce/pricing/")
+	if err != nil {
+		log.Fatalln("Failed to fetch EMR pricing data", err)
+	}
+
+	for regionName, regionSlug := range regionsInverted {
+		nameFormatting := strings.ReplaceAll(
+			regionName,
+			" ",
+			"_",
+		)
+		nameFormatting = strings.ReplaceAll(
+			nameFormatting,
+			"(",
+			".28",
+		)
+		nameFormatting = strings.ReplaceAll(
+			nameFormatting,
+			")",
+			".29",
+		)
+
+		h2 := doc.Find("h2", "id", nameFormatting+"_Region")
+		if h2.Error != nil {
+			continue
+		}
+
+		// Find the item after it that has the class "lb-tbl"
+		var tableContainer *soup.Root = &h2
+		for {
+			table1 := tableContainer.FindNextElementSibling()
+			if table1.Error != nil {
+				tableContainer = nil
+				break
+			}
+			tableContainer = &table1
+			if strings.Contains(tableContainer.Attrs()["class"], "lb-tbl") {
+				break
+			}
+		}
+
+		if tableContainer == nil {
+			continue
+		}
+
+		table := tableContainer.Find("table")
+		if table.Error != nil {
+			continue
+		}
+
+		tbody := table.Find("tbody")
+		if tbody.Error != nil {
+			continue
+		}
+
+		trs := tbody.FindAll("tr")
+		if len(trs) < 2 {
+			continue
+		}
+		trs = trs[1:]
+
+		for _, tr := range trs {
+			tds := tr.FindAll("td")
+			if len(tds) < 3 {
+				continue
+			}
+			instanceType := tds[0].FullText()
+			priceString := tds[2].FullText()
+
+			// Find the float in the price string
+			matches := EMR_PRICE_REGEX.FindStringSubmatch(priceString)
+			if len(matches) == 0 {
+				continue
+			}
+			price := matches[0]
+
+			instance := instances[instanceType]
+			if instance == nil {
+				log.Default().Println("WARNING: EMR pricing data has unknown instance type", instanceType)
+				continue
+			}
+			regionPricing := instance.Pricing[regionSlug]
+			if regionPricing == nil {
+				regionPricing = make(map[OS]any)
+				instance.Pricing[regionSlug] = regionPricing
+			}
+			regionPricing["emr"] = &EC2PricingData{
+				EMR: price,
+			}
+			instance.EMR = true
+		}
+	}
 }
 
 const EMR_INSTANCE_TYPE_PREFIX = "Instance-instancetype-"
 
 func addEmrPricingUs(instances map[string]*EC2Instance, regionsInverted map[string]string) {
-	log.Default().Println("Adding EMR pricing to EC2")
+	log.Default().Println("Adding EMR pricing to EC2 (other regions)")
 
 	var emrData emrData
 	err := awsutils.FetchDataFromAWSWebsite(
