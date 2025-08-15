07441d1960aaa10cc8c2229299deb800577c65d6
Astrid Gealer <astrid@gealer.email>
2025-08-15 09:07:43 +0100

    add timeout and retry count for aws china site

--- scraper/utils/load_html.go
+++ scraper/utils/load_html.go
@@ -1,15 +1,26 @@
 package utils
 
 import (
+	"context"
 	"io"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/anaskhan96/soup"
 )
 
-// LoadHTML loads an HTML document from a URL and returns the root node
-func LoadHTML(url string) (*soup.Root, error) {
-	resp, err := http.Get(url)
+func tryWith2MinTimeout(url string) (*soup.Root, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	
 	if err != nil {
 		return nil, err
 	}
@@ -26,4 +37,21 @@ func LoadHTML(url string) (*soup.Root, error) {
 	}
 
 	return &val, nil
+}
+
+// LoadHTML loads an HTML document from a URL and returns the root node
+func LoadHTML(url string) (*soup.Root, error) {
+	tries := 0
+	var val *soup.Root
+	var err error
+	for tries < 3 {
+		val, err = tryWith2MinTimeout(url)
+		if err == nil {
+			return val, nil
+		}
+		tries++
+		time.Sleep(10 * time.Second)
+		log.Printf("Failed to load HTML from %s, retrying... (try %d)", url, tries)
+	}
+	return nil, err
 }
