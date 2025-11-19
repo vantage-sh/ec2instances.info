2feccfe0f449afbca9882db112493e2b3e95549b
Astrid Gealer <astrid@gealer.email>
2025-11-19 22:28:22 +0000

    add image ability to toaster

--- next/components/AdToasts.tsx
+++ next/components/AdToasts.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { array, InferOutput, object, string, parse } from "valibot";
+import { array, InferOutput, object, string, parse, optional } from "valibot";
 import { toast } from "sonner";
 import { browserBlockingLocalStorage } from "@/utils/abGroup";
+import { Button } from "./ui/button";
 
 const toastsSchema = array(
     object({
         campaign_id: string(),
         message: string(),
+        image_alt_text: optional(string()),
+        image_url: optional(string()),
         url: string(),
     }),
 );
@@ -21,6 +24,83 @@ function setUnlessBlockingLocalStorage(key: string, value: string) {
     }
 }
 
+function ToastInner({
+    idContainer,
+    message,
+    imageUrl,
+    imageAltText,
+    visit,
+}: {
+    idContainer: [number | string | null];
+    message: string;
+    imageUrl?: string;
+    imageAltText?: string;
+    visit?: () => void;
+}) {
+    let title: string | undefined = undefined;
+    let description = message;
+    const idx = message.indexOf("\n");
+    if (idx !== -1) {
+        title = message.slice(0, idx);
+        description = message.slice(idx + 1);
+    }
+
+    let element = (
+        <p>
+            {description.split("\n").map((line, i) => (
+                <span key={i}>
+                    {line}
+                    <br />
+                </span>
+            ))}
+        </p>
+    );
+    if (title) {
+        element = (
+            <>
+                <p className="font-bold mb-1">{title}</p>
+                {element}
+            </>
+        );
+    }
+
+    if (imageUrl) {
+        element = (
+            <>
+                <img
+                    src={imageUrl}
+                    alt={imageAltText || ""}
+                    className="mb-2 max-h-32 w-auto mx-auto"
+                />
+                {element}
+            </>
+        );
+    }
+
+    if (visit) {
+        element = (
+            <div className="block">
+                <div className="mb-4">{element}</div>
+                <div className="flex justify-end">
+                    <Button
+                        size="sm"
+                        onClick={() => {
+                            visit();
+                            if (idContainer[0] !== null) {
+                                toast.dismiss(idContainer[0]);
+                            }
+                        }}
+                    >
+                        Learn More
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+
+    return element;
+}
+
 function processToasts(toasts: Toasts) {
     for (const t of toasts.slice().reverse()) {
         // Check if the user dismissed this
@@ -31,35 +111,40 @@ function processToasts(toasts: Toasts) {
             continue;
         }
 
-        // Get the title and description.
-        let title = t.message;
-        let description: string | undefined = undefined;
-        if (title.includes("\n")) {
-            [title, description] = title.split("\n", 2);
-        }
-
-        // Show the toast
-        toast(title, {
-            description,
-            onDismiss: () => {
-                setUnlessBlockingLocalStorage(
-                    `toastDismissed-${t.campaign_id}`,
-                    "true",
-                );
-            },
-            action: {
-                onClick: () => {
-                    window.open(t.url, "_blank");
+        // Show the toast. We have to do some janky circular stuff here to get the ID to the toast
+        const idContainer: [number | string | null] = [null];
+        const id = toast.message(
+            () => (
+                <ToastInner
+                    idContainer={idContainer}
+                    message={t.message}
+                    imageUrl={t.image_url}
+                    imageAltText={t.image_alt_text}
+                    visit={() => {
+                        window.open(t.url, "_blank");
+                        setUnlessBlockingLocalStorage(
+                            `toastDismissed-${t.campaign_id}`,
+                            "true",
+                        );
+                    }}
+                />
+            ),
+            {
+                onDismiss: () => {
                     setUnlessBlockingLocalStorage(
                         `toastDismissed-${t.campaign_id}`,
                         "true",
                     );
                 },
-                label: "Visit",
+                closeButton: true,
+                duration: Infinity,
+
+                // This is weird, but it is needed to fix some weird
+                // state update issues with sonner
+                action: "",
             },
-            closeButton: true,
-            duration: Infinity,
-        });
+        );
+        idContainer[0] = id;
     }
 }
 
